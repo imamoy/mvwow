7e7e661e736e0853f486544e0bca29b896c3dc80
Moy Wu <immoywu@gmail.com>
2019-04-13 21:11:41 +0800

    no message

--- js/header.js
+++ js/header.js
@@ -1,13 +1,15 @@
 $(function () {
-	$(window).scroll(function () {
-		if ($(this).scrollTop() > 10) { /* 要滑動到選單的距離 */
-			$('header .menu').addClass('navfix'); /* 幫選單加上固定效果 */
-			$('header .menu .logo').removeClass('hide');
-		} else {
-			$('header .menu').removeClass('navfix'); /* 移除選單固定效果 */
-			$('header .menu.logo').addClass('hide');
-		}
-	});
+	if ($(window).width() > 640) { 
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 10) { /* 要滑動到選單的距離 */
+				$('header .menu').addClass('navfix'); /* 幫選單加上固定效果 */
+				$('header .menu .logo').removeClass('hide');
+			} else {
+				$('header .menu').removeClass('navfix'); /* 移除選單固定效果 */
+				$('header .menu.logo').addClass('hide');
+			}
+		});
+	}
 
 	$(".menu-toggle").click(function() {
 		$(this).toggleClass('active');
@@ -17,6 +19,7 @@ $(function () {
 	if ($(window).width() < 641) { 
 		$('header .menu .sub-toggle').click(function(){
 			$(this).find('.sub-nav').slideToggle().parent().siblings().find('.sub-nav').slideUp();
+			$('.main-header').toggleClass('active');
 		});
 		$('header .search-toggle').click(function(){
 			$(this).next('.search').slideToggle();
